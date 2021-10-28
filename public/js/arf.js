1d0446683122b962e3f052568a219c638c1f7183
William Cooper <claws.delayer.0b@icloud.com>
2021-10-28 22:54:29 +0100

    Update arf.js

--- public/js/arf.js
+++ public/js/arf.js
@@ -61,7 +61,7 @@ function update(source) {
 
   nodeEnter.append("svg:circle")
       .attr("r", 1e-6)
-      .style("fill", function(d) { return d._children ? "magenta" : "yellow"; });
+      .style("fill", function(d) { return d._children ? "indigo" : "yellow"; });
 
   nodeEnter.append('a')
       .attr("target", "_blank")
@@ -86,7 +86,7 @@ function update(source) {
 
   nodeUpdate.select("circle")
       .attr("r", 6)
-      .style("fill", function(d) { return d._children ? "magenta" : "yellow"; });
+      .style("fill", function(d) { return d._children ? "indigo" : "yellow"; });
 
   nodeUpdate.select("text")
       .style("fill-opacity", 1);
