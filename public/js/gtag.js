1901d3983a3a8f8838abcbb173134a55e8d00494
Salvydas Lukosius <sall@w-ss.io>
2021-11-02 09:46:51 +0000

    Delete gtag.js

--- public/js/gtag.js
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-LMM3QM7TDT"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-LMM3QM7TDT');
-</script>
